30aa53487a9f7fe798d42ef599fa9097adeea509
Ali-Al-Ahmad <ali.alahmad.cs@gmail.com>
2024-11-13 22:35:31 +0200

    clean code

--- special-rearrangement.py
+++ special-rearrangement.py
@@ -1,19 +1,20 @@
-numbers_array = [1,2,5,8,3,6,9,4,7,10]
-#               [2,8,6,4,10,1,5,3,9,7]
+"this function rearrange a list of numbers, even number to left side/odd number to right side"
 
-length_of_array = len(numbers_array)
+numbers_array = [1, 2, 5, 8, 3, 6, 9, 4, 7, 10]
+#               [2, 8, 6, 4, 10, 1, 5, 3, 9, 7]
 
-def special_rearrangement(numbers):
-    
+def _special_rearrangement(numbers):
+    length_of_array = len(numbers)
+
     for i in range(length_of_array):
         for j in range(length_of_array-1):
-            
+
             # check if number is odd move it to the right (index + 1)
-            if numbers[j] %2== 1:
+            if numbers[j] % 2 == 1:
                 temp = numbers[j+1]
                 numbers[j+1] = numbers[j]
                 numbers[j] = temp
-    return numbers 
+    return numbers
 
-new_array = special_rearrangement(numbers_array)   
-print(new_array)      
+new_array = _special_rearrangement(numbers_array)
+print(new_array)
